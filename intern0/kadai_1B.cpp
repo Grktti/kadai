58a222157884d4ebd88ff74ed903334ff5ab8a17
r.kato <21eh019@ms.dendai.ac.jp>
2025-08-18 20:06:58 +0900

    課題２Bまで作成完了

--- intern0/kadai_1B.cpp
+++ intern0/kadai_1B.cpp
@@ -9,6 +9,7 @@
 
 // 平均: 読み取り専用の参照で無駄なコピーを避ける
 double meanOf(const std::vector<int>& v){
+    // TODO: 平均を計算する関数を作成
     if (v.empty()) throw std::invalid_argument("meanOf: empty vector");
     long long sum = std::accumulate(v.begin(), v.end(), 0LL);
     return static_cast<double>(sum) / static_cast<double>(v.size());
@@ -16,12 +17,14 @@ double meanOf(const std::vector<int>& v){
 
 // 最大: イテレータから値を取り出す
 int maxOf(const std::vector<int>& v){
+    // TODO: 最大値を計算する関数を作成
     if (v.empty()) throw std::invalid_argument("maxOf: empty vector");
     return *std::max_element(v.begin(), v.end());
 }
 
 // 中央値: 呼び出し側のデータを壊さないよう値渡しでコピー
 double medianOf(std::vector<int> v){
+    // TODO: 中央値を計算する関数を作成
     if (v.empty()) throw std::invalid_argument("medianOf: empty vector");
     std::sort(v.begin(), v.end());
     size_t n = v.size();
